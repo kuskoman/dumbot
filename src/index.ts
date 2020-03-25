b624fe779e2c36a2a7ae9156ac2d47dc4d77c8ea
kkm <kubasurdej@gmail.com>
2020-03-25 15:06:04 +0100

    Handle messages

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import "dotenv/config";
-import { Client as DiscordClient } from "discord.js";
+import { Client as DiscordClient, PartialMessage, Message } from "discord.js";
 
 const client = new DiscordClient();
+const prefix = "?";
 client.on("ready", () => {
   console.log(`Bot logged in as ${client.user.tag}`);
 });
 
+const handleMessage = (msg: Message | PartialMessage) => {
+  const content = msg.content.slice(prefix.length);
+  const reply = (text: string) => {
+    msg.channel.send(text);
+  };
+
+  if (content === "sei") {
+    reply("cento");
+  }
+};
+
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+  if (msg.content.startsWith(prefix)) {
+    handleMessage(msg);
   }
 });
 
