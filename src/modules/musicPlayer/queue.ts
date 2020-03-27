bc1f43546fca43a6bb0597214b6621d1a5f4a6c7
kkm <kubasurdej@gmail.com>
2020-03-27 23:57:11 +0100

    Add lastSong property to queue

--- src/modules/musicPlayer/queue.ts
+++ src/modules/musicPlayer/queue.ts
@@ -2,13 +2,16 @@ import { Song } from "./song";
 
 export class MusicQueue {
   public songList: Song[] = [];
+  public lastSong: Song;
 
   public addSong(song: Song) {
     this.songList.push(song);
   }
 
   public getSong(): Song {
-    return this.songList.shift();
+    const song = this.songList.shift();
+    this.lastSong = song;
+    return song;
   }
 
   public removeSong(index) {
