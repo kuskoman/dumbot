2bad0ae083403e8b484b645e863a0f0837b950a2
kkm <kubasurdej@gmail.com>
2020-03-29 15:54:28 +0200

    Add function for extracting yt links

--- src/modules/musicPlayer/yt.ts
+++ src/modules/musicPlayer/yt.ts
@@ -2,8 +2,26 @@ import youtubeSearch from "youtube-search";
 import { Msg } from "src/types";
 import { Song } from "./song";
 import { promisify } from "util";
+import { getInfo } from "ytdl-core";
 
-export const getSongDataFromYT = async (
+const YT_REGEX = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
+// regex source: https://stackoverflow.com/questions/3717115/regular-expression-for-youtube-links
+
+export const getSongData = async (msg: Msg, query: string): Promise<Song> => {
+  if (!query || query.length < 3) {
+    msg.channel.send("Query missing or query length shorter than 3.");
+    throw new Error("Query too short or not present");
+  }
+
+  const songData = await getSongDataFromYT(query);
+  const song = {
+    ...songData,
+    addedBy: msg.member.id
+  };
+  return song;
+};
+
+const getSongDataFromYT = async (
   query: string
 ): Promise<GetSongDataFromYTResponse> => {
   const opts: youtubeSearch.YouTubeSearchOptions = {
@@ -21,17 +39,13 @@ export const getSongDataFromYT = async (
   };
 };
 
-export const getSongData = async (msg: Msg, query: string): Promise<Song> => {
-  if (!query || query.length < 3) {
-    msg.channel.send("Query missing or query length shorter than 3.");
-    throw new Error("Query too short or not present");
+const extractYouTubeLink = (possibleLink: string): string | undefined => {
+  const matches = possibleLink.match(YT_REGEX);
+  if (matches && matches[1]) {
+    const id = matches[1];
+    return `https://www.youtube.com/watch?v=${id}`;
   }
-  const songData = await getSongDataFromYT(query);
-  const song = {
-    ...songData,
-    addedBy: msg.member.id
-  };
-  return song;
+  return undefined;
 };
 
 export interface GetSongDataFromYTResponse {
