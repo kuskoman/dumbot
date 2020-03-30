fc2ef1a8fde668456af22e7d7ef675c5f04b8496
kkm <kubasurdej@gmail.com>
2020-03-30 15:16:54 +0200

    Return to old version of leave command

--- src/modules/musicPlayer/commands/leave.ts
+++ src/modules/musicPlayer/commands/leave.ts
@@ -1,11 +1,14 @@
 import { CommandOpts, Command } from "../../../module";
+import { MusicPlayer } from "../player";
 
 export class LeaveCommand implements Command {
   public name = "leave";
   public patterns = ["leave", "l"];
   public execute({ msg }: CommandOpts) {
     msg.member.voice.channel.leave();
-    // bot should detect 'close' event and end voice stream instantly
+    const player = MusicPlayer.get(msg.guild.id);
+    player.queue.reset();
+    player.dispatcher.end();
   }
 }
 
