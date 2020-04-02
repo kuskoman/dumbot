4e03a3a7c50818c1a47fd4c14d0da45d49d95659
kkm <kubasurdej@gmail.com>
2020-04-02 16:33:16 +0200

    Add required imports to new soundPlayer

--- src/modules/soundPlayer/index.ts
+++ src/modules/soundPlayer/index.ts
@@ -1,11 +1,18 @@
 import { DumbotModule } from "../../module";
 import loader from "../../moduleLoader";
+import radio from "./plugins/radio/commands/radio";
+import clearQueue from "./commands/clearQueue";
+import current from "./commands/current";
+import join from "./commands/join";
+import leave from "./commands/leave";
+import queue from "./commands/queue";
+import skip from "./commands/skip";
 
 export class SoundPlayerModule implements DumbotModule {
   public name = "DefaultModule";
   public description = "Module used for playing music from YouTube";
   public version = "1.0.0";
-  public commands = [];
+  public commands = [clearQueue, current, join, leave, queue, skip, radio];
 
   constructor() {
     loader.registerModule(this);
