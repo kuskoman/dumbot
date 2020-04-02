38942066fc366417cbbdf48c7135a5ddec9df48a
kkm <kubasurdej@gmail.com>
2020-04-02 16:24:06 +0200

    Modify song interface

--- src/modules/soundPlayer/song.ts
+++ src/modules/soundPlayer/song.ts
@@ -1,14 +1,14 @@
-import { VoiceBroadcast, StreamOptions } from "discord.js";
+import { VoiceBroadcast, StreamOptions, StreamDispatcher } from "discord.js";
 import internal from "stream";
 
 export type YouTubeID = string;
 export type DiscordID = string;
 
 export interface Song {
-  name: string;
-  id: YouTubeID;
+  name?: string;
+  id?: YouTubeID;
   addedBy: DiscordID;
-  uri: PlayableResource;
+  getStream: () => PlayableResource;
   options?: StreamOptions;
 }
 
