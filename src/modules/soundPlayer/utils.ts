18d4608f838e44c4ae4e0aaac419637f48064e95
kkm <kubasurdej@gmail.com>
2020-04-02 21:17:45 +0200

    Add catch to error when bot is trying to join channel withoutpermission

--- src/modules/soundPlayer/utils.ts
+++ src/modules/soundPlayer/utils.ts
@@ -1,12 +1,26 @@
 import { Msg } from "../../types";
 
 export const joinChannel = async (msg: Msg) => {
-  const channel = msg.member.voice.channel;
-  if (channel) {
+  const channel = msg.member?.voice?.channel;
+  if (!channel) {
+    msg.channel.send("You must be in a voice channel first");
+    return false;
+  }
+
+  if (!channel.guild.me.hasPermission("CONNECT")) {
+    msg.channel.send("Bot does not have permission to join this channel.");
+    return false;
+  }
+
+  try {
     await channel.join();
     console.log(`Joined channel ${channel.id}`);
+
     return channel;
+  } catch (e) {
+    msg.channel.send("Could not join channel. Check permissions.");
+    console.log(`Could not join channel. Error: ${e}`);
+
+    return false;
   }
-  msg.channel.send("You must be in a voice channel first");
-  return false;
 };
