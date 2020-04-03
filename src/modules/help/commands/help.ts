5aebb10c802474d8b95259df13860c2f4742055e
kkm <kubasurdej@gmail.com>
2020-04-03 14:20:17 +0200

    Add more aliases to help command

--- src/modules/help/commands/help.ts
+++ src/modules/help/commands/help.ts
@@ -3,14 +3,16 @@ import { getModulesHelp, getCommandsHelp, getHelp } from "../getHelp";
 
 export class HelpCommand implements Command {
   public name = "help";
-  public patterns = ["help", "h", "module", "command"];
+  public patterns = ["help", "h", "module", "modules", "command", "commands"];
   public execute({ msg, command, args }: CommandOpts) {
     const query = args.trim();
     switch (command) {
       case "module":
+      case "modules":
         msg.channel.send(getModulesHelp(query));
         break;
       case "command":
+      case "commands":
         msg.channel.send(getCommandsHelp(query));
         break;
       default:
