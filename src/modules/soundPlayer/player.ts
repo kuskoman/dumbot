7321022a8ed3366512635b88277043db38fef68e
kkm <kubasurdej@gmail.com>
2020-04-04 17:36:52 +0200

    Use this.endDispatcher() instead of this.dispatcher.end()

--- src/modules/soundPlayer/player.ts
+++ src/modules/soundPlayer/player.ts
@@ -40,7 +40,7 @@ export class SoundPlayer {
 
     if (mode === "radio") {
       this.isQueuePaued = true;
-      this.dispatcher.end();
+      this.endDispatcher();
       msg.channel.send(`Playing radio **${song.name}**.`);
       return this.streamSong({ textChannel, connection, song, mode });
     }
@@ -65,7 +65,7 @@ export class SoundPlayer {
 
     msg.channel.send("Skipping current song");
     this.isQueuePaued = false;
-    this.dispatcher.end();
+    this.endDispatcher();
   }
 
   private streamSong({ connection, textChannel, song }: StreamSongOpts) {
