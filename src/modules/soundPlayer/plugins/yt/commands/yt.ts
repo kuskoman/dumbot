f4125ecef70e2b31813a0526c1cb0f37a84846b0
kkm <kubasurdej@gmail.com>
2020-04-04 17:55:02 +0200

    Fix type errors in yt plugin

--- src/modules/soundPlayer/plugins/yt/commands/yt.ts
+++ src/modules/soundPlayer/plugins/yt/commands/yt.ts
@@ -5,9 +5,16 @@ import { getSongFromYouTube } from "..";
 export class PlayYTCommand implements Command {
   public name = "playYT";
   public patterns = ["playyt", "yt"];
-  public async execute({ msg, args }: CommandOpts) {
+  public async execute({ msg, args }: CommandOpts): Promise<any> {
     const song = await getSongFromYouTube(msg, args);
+    if (!msg.guild?.id) {
+      return msg.channel.send("Can't find server id.");
+    }
     const player = SoundPlayer.get(msg.guild.id);
+    if (!song) {
+      return msg.channel.send(`Error when fetching song ${args}`);
+    }
+
     player.play({ msg, song });
   }
 }
--- src/modules/soundPlayer/plugins/yt/search.ts
+++ src/modules/soundPlayer/plugins/yt/search.ts
@@ -3,19 +3,24 @@ import { promisify } from "util";
 
 export const getSongDataFromYT = async (
   query: string
-): Promise<GetSongDataFromYTResponse> => {
+): Promise<GetSongDataFromYTResponse | undefined> => {
   const opts: youtubeSearch.YouTubeSearchOptions = {
     maxResults: 1,
-    key: process.env.YT_API_KEY
+    key: process.env.YT_API_KEY,
   };
 
   const search = promisify(youtubeSearch);
   const response = await search(query, opts);
+
+  if (!response) {
+    return undefined;
+  }
+
   const song = response[0];
   return {
     name: song.title,
     id: song.id,
-    link: song.link
+    link: song.link,
   };
 };
 
