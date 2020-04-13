3ab3acbcf1745db9b0575d8e0ef4fa2e02c0895a
kkm <kubasurdej@gmail.com>
2020-04-13 20:13:11 +0200

    Fix type violations in clearQueue

--- src/modules/soundPlayer/commands/clearQueue.ts
+++ src/modules/soundPlayer/commands/clearQueue.ts
@@ -4,7 +4,10 @@ import { SoundPlayer } from "../player";
 export class ClearQueueCommand implements Command {
   public name = "stop";
   public patterns = ["clear", "cq", "clear queue", "clearqueue"];
-  public execute({ msg }: CommandOpts) {
+  public execute({ msg }: CommandOpts): any {
+    if (!msg.guild?.id) {
+      return msg.channel.send("Can't find server id.");
+    }
     const player = SoundPlayer.get(msg.guild.id);
     player.queue.reset();
     msg.channel.send("Queue cleared");
