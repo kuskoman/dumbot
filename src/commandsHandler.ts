3dac16f032af936feae12683a4398675b1377435
kkm <kubasurdej@gmail.com>
2020-04-13 20:10:24 +0200

    Fix type violations in commandsHandler

--- src/commandsHandler.ts
+++ src/commandsHandler.ts
@@ -4,24 +4,29 @@ import loader from "./moduleLoader";
 import logger from "./logger";
 
 export const handleMessage = async (msg: Msg) => {
-  const command = extractCommand(msg).toLowerCase();
+  const command = extractCommand(msg);
   const args = extractArgs(msg);
 
+  if (!command || !args) {
+    return;
+  }
+
+  const lowercaseCmd = command.toLowerCase();
   const commands = loader.commands;
   commands.forEach((c) => {
-    if (c.patterns.includes(command)) {
+    if (c.patterns.includes(lowercaseCmd)) {
       logger.debug(
         `Message ${msg.content} recognised as command. Executing using ${c.name}`
       );
-      return c.execute({ msg, command, args });
+      return c.execute({ msg, command: lowercaseCmd, args });
     }
   });
 };
 
 const extractCommand = (msg: Msg) => {
-  return msg.content.slice(PREFIX.length).split(" ")[0];
+  return msg.content?.slice(PREFIX.length).split(" ")[0];
 };
 
 const extractArgs = (msg: Msg) => {
-  return msg.content.slice(PREFIX.length).split(" ").slice(1).join(" ");
+  return msg.content?.slice(PREFIX.length).split(" ").slice(1).join(" ");
 };
