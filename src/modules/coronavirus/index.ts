22e006e7cc21494a6be6929fc9e5c2c5ca5391dd
kkm <kubasurdej@gmail.com>
2020-04-16 20:18:34 +0200

    Add covid command to coronavirus module

--- src/modules/coronavirus/index.ts
+++ src/modules/coronavirus/index.ts
@@ -1,11 +1,12 @@
 import { DumbotModule } from "../../module";
 import loader from "../../moduleLoader";
+import covid from "./commands/covid";
 
 export class CovidApiModule implements DumbotModule {
   public name = "Covid API Client";
   public description = "Provides stats of coronavirus cases";
   public version = "1.0.0";
-  public commands = [];
+  public commands = [covid];
 
   constructor() {
     loader.registerModule(this);
