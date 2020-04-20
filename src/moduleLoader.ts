2d6bc2d947f0b162577fff650f7ce33f7231e6c0
kkm <kubasurdej@gmail.com>
2020-04-20 15:48:40 +0200

    Log loading module

--- src/moduleLoader.ts
+++ src/moduleLoader.ts
@@ -1,10 +1,12 @@
 import { DumbotModule, Command } from "./module";
+import logger from "./logger";
 
 export class ModuleLoader {
   public modules: DumbotModule[] = [];
   public commandPatterns: Map<string, Command> = new Map();
 
   public registerModule(module: DumbotModule) {
+    logger.debug(`Registering module ${module.name}`);
     this.modules.push(module);
     module.commands.forEach((command) => {
       command.patterns.forEach((pattern) => {
