43e27b391628542e88dc6a5fb8799fc559dc65d2
kkm <kubasurdej@gmail.com>
2020-04-20 19:33:09 +0200

    Write scaffold of test for commandsHandler

---
+++ test/commandsHandler.test.ts
@@ -0,0 +1,16 @@
+import { handleMessage } from "../src/commandsHandler";
+import { Msg } from "../src/types";
+import { PREFIX } from "../src";
+import moduleLoader from "../src/moduleLoader";
+
+describe("commandsHandler", () => {
+  moduleLoader.getCommand = jest.fn();
+
+  const mockedMsg = {
+    content: `${PREFIX}testcommand args`,
+  };
+  it("handles commands properly", () => {
+    handleMessage(mockedMsg as Msg);
+    expect(moduleLoader.getCommand).toBeCalledWith("testcommand");
+  });
+});
