2162bf135faf0b83c27ed1fb87c7fd7613859185
kkm <kubasurdej@gmail.com>
2020-04-22 20:19:52 +0200

    Add checks for presence of environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import "dotenv/config";
 import "./modules";
 import bot from "./bot";
+import logger from "./logger";
+import config from "./config";
+
+if (!process.env.DISCORD_TOKEN) {
+  logger.error("Missing DISCORD_TOKEN environment variable");
+}
+if (!process.env.PREFIX) {
+  logger.warn(
+    `Missing PREFIX environemnt variable. Using default ${config.prefix} instead`
+  );
+}
+
+if (!process.env.YT_API_KEY) {
+  logger.warn(
+    `Missing YT_API_KEY environemnt variable. Playing songs from YouTube will be disabled`
+  );
+}
 
 bot.login(process.env.DISCORD_TOKEN);
