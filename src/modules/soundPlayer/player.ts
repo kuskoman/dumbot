4eff58ea70796f2e6189dae730f490afee660a48
kkm <kubasurdej@gmail.com>
2020-05-05 17:09:54 +0200

    Add console.logs for debugging connection dispatcher events

--- src/modules/soundPlayer/player.ts
+++ src/modules/soundPlayer/player.ts
@@ -72,7 +72,14 @@ export class SoundPlayer {
         logger.info(`Bot playing ${song.name} on ${connection.channel.id}.`);
       })
       .on("finish", () => {
+        console.log("finish");
         this.handleSongEnd(connection);
+      })
+      .on("close", () => {
+        console.log("close");
+      })
+      .on("error", () => {
+        console.log("error");
       });
   }
 
